4ed8951013f28a11f94958d4a3d15870126eb2e0
pir0zhki <94156622+pir0zhki@users.noreply.github.com>
2023-05-24 00:38:03 +0900

    Update starting3.js

--- starting3.js
+++ starting3.js
@@ -17,6 +17,8 @@ function formatJSON(json){
     let sun;
     let Month=[];
     let year=[ap_weeks,may_weeks,june_weeks];
+    
+    let result=[];
 console.log(year[0]);
 console.log('Fri[2]の中身'+year[0][0].Fri[2]);
 console.log(year[0]);
@@ -26,92 +28,115 @@ console.log(year[0][0]);
     //内側のネストi:第何週か
     //switch:m月の第i週の各曜日の2番目の要素(勝敗)
     for(var m=0;m<3;m++){
+        let win=0;
+        let lose=0;
         for(var i=0;i<5;i++){
             switch(year[m][i].Mon[2]){
-                case '〇':
-                mon= `<td style="background-color: rgba(255,0,255,0.6)">${year[m][i].Mon[0]}<br>${year[m][i].Mon[1]}<br>${year[m][i].Mon[2]}</td>`;
-                    break;
-                case '×':
+                case '〇':{
+                    mon= `<td style="background-color: rgba(255,0,255,0.6)">${year[m][i].Mon[0]}<br>${year[m][i].Mon[1]}<br>${year[m][i].Mon[2]}</td>`;
+                    win+=1;
+                    break}
+                case '×':{
                     mon=`<td style="background-color: rgba(138,43,226,0.6)">${year[m][i].Mon[0]}<br>${year[m][i].Mon[1]}<br>${year[m][i].Mon[2]}</td>`;
-                    break;
+                    lose+=1;
+                    break}
                 default:
                     mon=`<td>${year[m][i].Mon[0]}<br>${year[m][i].Mon[1]}<br>${year[m][i].Mon[2]}</td>`;
                     break;
                 }
             switch(year[m][i].Tue[2]){
-                case '〇':
+                case '〇':{
                     tue=`<td style="background-color: rgba(255,0,255,0.6)">${year[m][i].Tue[0]}<br>${year[m][i].Tue[1]}<br>${year[m][i].Tue[2]}</td>`;
-                    break;
-                case '×':
+                    win+=1;
+                    break}
+                case '×':{
                     tue=`<td style="background-color: rgba(138,43,226,0.6)">${year[m][i].Tue[0]}<br>${year[m][i].Tue[1]}<br>${year[m][i].Tue[2]}</td>`;
-                    break;
+                    lose+=1;
+                    break}
                 default:
                     tue=`<td>${year[m][i].Tue[0]}<br>${year[m][i].Tue[1]}<br>${year[m][i].Tue[2]}</td>`;
                     break;
                 }
             switch(year[m][i].Wed[2]){
-                case '〇':
+                case '〇':{
                     wed=`<td style="background-color: rgba(255,0,255,0.6)">${year[m][i].Wed[0]}<br>${year[m][i].Wed[1]}<br>${year[m][i].Wed[2]}</td>`;
-                    break;
-                case '×':
+                    win+=1;
+                    break}
+                case '×':{
                     wed=`<td style="background-color: rgba(138,43,226,0.6)">${year[m][i].Wed[0]}<br>${year[m][i].Wed[1]}<br>${year[m][i].Wed[2]}</td>`;
-                    break;
+                    lose+=1;
+                    break}
                 default:
                     wed=`<td>${year[m][i].Wed[0]}<br>${year[m][i].Wed[1]}<br>${year[m][i].Wed[2]}</td>`;
                     break;
                 }
             switch(year[m][i].Thu[2]){
-                case '〇':
+                case '〇':{
                     thu=`<td id='win' style="background-color: rgba(255,0,255,0.6)">${year[m][i].Thu[0]}<br>${year[m][i].Thu[1]}<br>${year[m][i].Thu[2]}</td>`;
-                    break;
-                case '×':
+                    win+=1;
+                    break}
+                case '×':{
                     thu=`<td style="background-color: rgba(138,43,226,0.6)">${year[m][i].Thu[0]}<br>${year[m][i].Thu[1]}<br>${year[m][i].Thu[2]}</td>`;
-                    break;
+                    lose+=1;
+                    break}
                 default:
                     thu=`<td>${year[m][i].Thu[0]}<br>${year[m][i].Thu[1]}<br>${year[m][i].Thu[2]}</td>`;
                     break;
                 }
             switch(year[m][i].Fri[2]){
-                case '〇':
+                case '〇':{
                     fri=`<td style="background-color: rgba(255,0,255,0.6)">${year[m][i].Fri[0]}<br>${year[m][i].Fri[1]}<br>${year[m][i].Fri[2]}</td>`;
-                    break;
-                case '×':
+                    win+=1;
+                    break}
+                case '×':{
                     fri=`<td style="background-color: rgba(138,43,226,0.6)">${year[m][i].Fri[0]}<br>${year[m][i].Fri[1]}<br>${year[m][i].Fri[2]}</td>`;
-                    break;
+                    lose+=1;
+                    break}
                 default:
                     fri=`<td>${year[m][i].Fri[0]}<br>${year[m][i].Fri[1]}<br>${year[m][i].Fri[2]}</td>`;
                     break;
                 }
             switch(year[m][i].Sat[2]){
-                case '〇':
+                case '〇':{
                     sat=`<td style="background-color: rgba(255,0,255,0.6)">${year[m][i].Sat[0]}<br>${year[m][i].Sat[1]}<br>${year[m][i].Sat[2]}</td>`;
-                    break;
-                case '×':
+                    win+=1;
+                    break}
+                case '×':{
                     sat=`<td style="background-color: rgba(138,43,226,0.6)">${year[m][i].Sat[0]}<br>${year[m][i].Sat[1]}<br>${year[m][i].Sat[2]}</td>`;
-                    break;
+                    lose+=1;
+                    break}
                 default:
                     sat=`<td>${year[m][i].Sat[0]}<br>${year[m][i].Sat[1]}<br>${year[m][i].Sat[2]}</td>`;
                     break;
                 }
             switch(year[m][i].Sun[2]){
-                case '〇':
+                case '〇':{
                     sun=`<td style="background-color: rgba(255,0,255,0.6)">${year[m][i].Sun[0]}<br>${year[m][i].Sun[1]}<br>${year[m][i].Sun[2]}</td>`;
-                    break;
-                case '×':
+                    win+=1;
+                    break}
+                case '×':{
                     sun=`<td style="background-color: rgba(138,43,226,0.6)">${year[m][i].Sun[0]}<br>${year[m][i].Sun[1]}<br>${year[m][i].Sun[2]}</td>`;
-                    break;
+                    lose+=1;
+                    break}
                 default:
                     sun=`<td>${year[m][i].Sun[0]}<br>${year[m][i].Sun[1]}<br>${year[m][i].Sun[2]}</td>`;
                     break;
                 }
             Month.push(mon+tue+wed+thu+fri+sat+sun);
-        };
-        
+        }
+        result.push(`<h2>${win}勝,${lose}敗</h2>`);
     }
+
     console.log('monthの中身:'+Month);
     console.log('month[1]の中身:'+Month[1]);
     console.log('month[0]の中身:'+Month[0]);
     console.log('month[15]の中身'+Month[15]);
+
+        document.getElementById("result4").innerHTML=result[0];
+        document.getElementById("result5").innerHTML=result[1];
+        document.getElementById("result6").innerHTML=result[2];
+
+
 
         document.getElementById("result4_1").innerHTML=Month[0];
         document.getElementById("result4_2").innerHTML=Month[1];
